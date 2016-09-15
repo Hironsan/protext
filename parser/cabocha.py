852c3e508f6aa6bc8d11915147a09650a5a779c4
h-nakayama <light.tree.1.13@gmail.com>
2016-09-15 20:54:20 +0900

    implement cabocha example

--- parser/cabocha.py
+++ parser/cabocha.py
@@ -1,1 +1,40 @@
-__author__ = 'h-nakayama'
+# -*- coding: utf-8 -*-
+import CaboCha
+
+
+c = CaboCha.Parser()
+
+sentence = '太郎はこの本を二郎を見た女性に渡した。'
+
+print(c.parseToString(sentence))
+
+tree = c.parse(sentence)
+
+print(tree.toString(CaboCha.FORMAT_TREE))
+print(tree.toString(CaboCha.FORMAT_LATTICE))
+print(tree.toString(CaboCha.FORMAT_XML))
+
+for i in range(tree.chunk_size()):
+    chunk = tree.chunk(i)
+    print('Chunk:', i)
+    print(' Score:', chunk.score)
+    print(' Link:', chunk.link)
+    print(' Size:', chunk.token_size)
+    print(' Pos:', chunk.token_pos)
+    print(' Head:', chunk.head_pos)  # 主辞
+    print(' Func:', chunk.func_pos)  # 機能語
+    print(' Features:',)
+    for j in range(chunk.feature_list_size):
+        print(chunk.feature_list(j),)
+    print()
+    print()
+
+for i in range(tree.token_size()):
+    token = tree.token(i)
+    print('Surface:', token.surface)
+    print(' Normalized:', token.normalized_surface)
+    print(' Feature:', token.feature)
+    print(' NE:', token.ne)  # 固有表現
+    print(' Info:', token.additional_info)
+    print(' Chunk:', token.chunk)
+    print()
